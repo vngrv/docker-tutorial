11c74d0d754e0609e22ff14e8b7a860fb793b662
Anastasiya Lukina <lukinanastya97@yandex.ru>
2020-06-25 20:51:41 +0300

    Исправен docker-compose.yml

--- students/k3343/practical_works/Lukina_Anastasia/docker/Stream/settings.py
+++ students/k3343/practical_works/Lukina_Anastasia/docker/Stream/settings.py
@@ -95,12 +95,23 @@ WSGI_APPLICATION = 'Stream.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'HOST': 'db',
+       'PORT': 5432,
+   }
 }
+
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
